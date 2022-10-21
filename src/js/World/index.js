2cb8ed34c143baf0edca1d04961944ca4e638713
romanechouteau <romane.chouteau@gmail.com>
2022-10-21 11:45:13 +0200

    :bug: lerp camera

--- src/js/World/index.js
+++ src/js/World/index.js
@@ -38,6 +38,8 @@ export default class World {
 		this.container = new Object3D()
 		this.container.name = 'World'
 
+		this.positionLerp = this.container.position.clone()
+
 		if (this.debug) {
 			this.debug.setFolder('Test')
 		}
@@ -187,12 +189,12 @@ export default class World {
 	}
 
 	updateWorldPosition() {
-		const currentX = Math.min(
+		this.positionLerp.x =  -Math.min(
 			this.playerManager.players[0].container.position.x,
 			this.playerManager.players[1].container.position.x
 		)
 
-		this.container.position.x = -currentX
+		this.container.position.lerp(this.positionLerp, 0.2)
 	}
 
 	updateTimer() {
